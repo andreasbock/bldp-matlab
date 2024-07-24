b084bb7d548191e8713ce5234966076904925664
andreasbock <aasbo@dtu.dk>
2024-07-24 22:02:19 +0200

    cleaned up script

--- scripts/RunPCGLarge_parallel.sh
+++ scripts/RunPCGLarge_parallel.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ## declare an array variable
-#declare -a names=("494_bus" "1138_bus") # testing
 declare -a names=("Pres_Poisson" "bmwcra_1" "cfd1" "smt" "apache1" "thermal1" "crankseg_1" "crankseg_2" "bcsstk17" "bcsstk18" "consph")
 
 ## now loop through the above array
