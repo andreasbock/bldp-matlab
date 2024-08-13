6e81082a30975e6cf644546f8fd13975414d2478
andreasbock <aasbo@dtu.dk>
2024-08-13 08:39:07 +0200

    new matrices

--- scripts/RunPCGLarge_parallel.sh
+++ scripts/RunPCGLarge_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 declare -a nystrom=("0" "1")
-declare -a names=("pdb1HYS" "bodyy4" "wathen100" "wathen120" "Dubcova1" "gyro_m" "apache1" "bmwcra_1" "crankseg_1" "cfd1" "thermal1")
+declare -a names=("Dubcova1" "gyro_m" "apache1" "crankseg_1" "cfd1" "thermal1")
 
 for j in "${nystrom[@]}"
 do
